af5fe2ad6e7c8c5c5ea78d60bb4277da634846c4
tomasy-j <tjanczyk@benaroyaresearch.org>
2025-03-09 20:13:57 -0700

    init commit

--- R/string_png.R
+++ R/string_png.R
@@ -1,36 +1,10 @@
-#' Get STRING .png for list of genes
-#'
-#' @param gene character vector of genes to plot on a STRING net
-#' @param species STRING taxon identifier, default homo sapiens \code{9606}
-#' @param png_filename path to write png
-#' @param label_font_size font size on save png, default \code{16}
-#' @param ... other arguments passed to \code{\link[httr:POST]{httr::POST()}}
-#' @importFrom httr POST write_disk
-#' @returns saved .png to \code{png_filename}
-#' @export
-get_string_png <- function(gene,
-                           species = 9606,
-                           png_filename,
-                           label_font_size = 16,
-                           ...) {
 
-  gene_list <- paste(
-    gene,
-    collapse = "%0d"
-  )
 
-  # instead, could let user put list of all STRING params
-  req_body_params <- list(
-    identifiers = gene_list,
-    species = species,
-    network_flavor = "confidence",
-    custom_label_font_size = label_font_size
-  )
 
-  httr::POST(
-    url = pullString:::base_img_url,
-    body = req_body_params,
-    httr::write_disk(png_filename, overwrite = TRUE),
-    ...
-  )
-}
+# r1 <- request(base_string_url) |>
+#   req_url_path_append(append_url_png_network) |>
+#   req_url_query(!!!params) |>
+#   req_user_agent(pullString:::usr_agent) |>
+#   req_perform() |>
+#   resp_body_raw()
+# readr::write_file(r1, "str_png234678.png")
--- README.md
+++ README.md
@@ -22,3 +22,17 @@ pullString::get_enr(
 )
 ```
 
+
+- Be considerate: when submitting requests in a `for` loop, it's considered a
+good practice to add `Sys.sleep(1)` after each call, so that server won't get
+overloaded.
+- Although STRING understands a variety of identifiers and does its best to
+disambiguate your input it is recommeded to map your identifier first (see:
+mapping). Querying the API with a disambiguated identifier (for example
+9606.ENSP00000269305 for human TP53) will guarantee much faster server response.
+- Another way to guarantee faster server response is to specify from which
+species your proteins come from (see 'species' parameter). In fact API will
+reject queries for networks larger than 10 proteins without the specified
+organism.
+- Current STRING version is 12.0 (https://version-12-0.string-db.org)
+
